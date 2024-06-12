8359fb2a3429760e9f160031605ef8d9d27783ff
isai66 <20211065@uthh.edu.mx>
2024-06-12 13:26:21 -0600

    Render 2

--- app.py
+++ app.py
@@ -1,42 +1,26 @@
-from flask import Flask, request, render_template, jsonify
-import joblib
-import pandas as pd
-import logging
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Configurar el registro
-logging.basicConfig(level=logging.DEBUG)
-
-# Cargar el modelo entrenado
-model = joblib.load('modelo.pkl')
-app.logger.debug('Modelo cargado correctamente.')
-
-@app.route('/')
-def home():
-    return render_template('formulario.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Obtener los datos enviados en el request
-        abdomen = float(request.form['abdomen'])
-        antena = float(request.form['antena'])
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    genre = None
+    if request.method == 'POST':
+        liveness = float(request.form['liveness'])
+        instrumentalness = float(request.form['instrumentalness'])
+        vocalness = float(request.form['vocalness'])
         
-        # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
-        app.logger.debug(f'DataFrame creado: {data_df}')
-        
-        # Realizar predicciones
-        prediction = model.predict(data_df)
-        app.logger.debug(f'Predicción: {prediction[0]}')
-        
-        # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': prediction[0]})
-    except Exception as e:
-        app.logger.error(f'Error en la predicción: {str(e)}')
-        return jsonify({'error': str(e)}), 400
+        # Lógica para determinar el género
+        if instrumentalness > 7:
+            genre = 'Instrumental'
+        elif vocalness > 7:
+            genre = 'Pop'
+        elif liveness > 7:
+            genre = 'Rock'
+        else:
+            genre = 'Clásica'
+
+    return render_template('index.html', genre=genre)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
