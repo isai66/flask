3e484539c8ac0da8b8e59ff00720df656e484f98
isai66 <20211065@uthh.edu.mx>
2024-06-26 13:29:22 -0600

    pry2

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    genre = None
-    if request.method == 'POST':
-        liveness = float(request.form['liveness'])
-        instrumentalness = float(request.form['instrumentalness'])
-        vocalness = float(request.form['vocalness'])
-        
-        # Lógica para determinar el género
-        if instrumentalness > 7:
-            genre = 'Instrumental'
-        elif vocalness > 7:
-            genre = 'Pop'
-        elif liveness > 7:
-            genre = 'Rock'
-        else:
-            genre = 'Clásica'
+# Cargar el modelo
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-    return render_template('index.html', genre=genre)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        latitud = float(request.form['latitud'])
+        longitud = float(request.form['longitud'])
+        num_cuartos = int(request.form['num_cuartos'])
+        num_habitaciones = int(request.form['num_habitaciones'])
+        
+        # Crear la matriz de entrada para la predicción
+        input_features = np.array([[latitud, longitud, num_cuartos, num_habitaciones]])
+        
+        # Hacer la predicción
+        prediction = model.predict(input_features)[0]
+        
+        return render_template('result.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
